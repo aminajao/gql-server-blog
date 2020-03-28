b82fad3694af4ffb035e75ad96d4f0b6e9a2ad3d
amin ajao <aminajao96@gmail.com>
2020-03-28 22:17:27 +0100

    users can create comments and delete them

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,10 +1,11 @@
-const { AuthenticationError } = require('apollo-server');
+const { AuthenticationError, UserInputError } = require('apollo-server');
 
 const Post = require('../../models/Post');
 const checkAuth = require('../../util/check-auth');
 
 module.exports = {
     Query: {
+
         async getPosts() {
             try {
                 const posts = await Post.find().sort({ createdAt: -1 });
@@ -14,6 +15,7 @@ module.exports = {
                 throw new Error(err); 
             }
         },
+
         async getPost(_, { postId}) {
             try {
                 const post = await Post.findById(postId);
@@ -30,6 +32,7 @@ module.exports = {
     },
 
     Mutation: {
+
         async createPost(_, { body }, context) {
             const user = checkAuth(context);
 
@@ -61,6 +64,35 @@ module.exports = {
             catch(err) {
                 throw new Error(err);
             };
+        },
+
+        async likePost(_, { postId }, context) {
+            const { userName } = checkAuth(context);
+
+            const post = await Post.findById(postId);
+
+            if(post) {
+                if(post.likes.find(like => like.userName === userName)) {
+                    // unlike post
+                    post.likes = post.likes.filter(like => like.userName !== userName);
+                }
+                else {
+                    //like post
+                    post.likes.push({
+                        userName,
+                        createdAt: new Date().toISOString()
+
+                    });
+                }
+                await post.save();
+                return post;
+            }
+            else {
+                throw new UserInputError('Post not found');
+            }
+
         }
+
+    
     }
 }
--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -7,6 +7,21 @@ module.exports = gql`
         body: String!
         createdAt: String!
         userName: String!
+        comments: [Comment]!
+        likes: [Like]!
+    }
+
+    type Comment {
+        id: ID!
+        body: String!
+        userName: String!
+        createdAt: String!
+    }
+
+    type Like {
+        id: ID!
+        createdAt: String!
+        userName: String!
     }
 
     type User {
@@ -34,5 +49,8 @@ module.exports = gql`
         login(userName: String!, password: String!): User!
         createPost(body: String!): Post!
         deletePost(postId: ID!): String!
+        createComment(postId: ID!, body: String!, ): Post!
+        deleteComment(postId: ID!, commentId: ID! ): Post!
+        likePost(postId: ID!): Post!
     }
 `
